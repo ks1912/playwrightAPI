50cf41b9de2c72df04c511f6a51620f278752b14
ks1912 <kunalsingh886312@gmail.com>
2023-09-04 13:44:29 +0530

    updated mock api

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 // app.js
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 app.use(cors());
 const port = 3000; // You can use any port you prefer
 
 // Define a route that returns a name
 app.get("/api/name", (req, res) => {
-  const name = "Kunal Singh"; // Replace this with your desired name or logic to fetch a name
-  res.json({ name });
+  const jsonData = { name: "Kunal Singh", id: "1" }; // Replace this with your desired name or logic to fetch a name
+  res.json(jsonData);
 });
 
 // Start the server
--- tests/api.spec.js
+++ tests/api.spec.js
@@ -2,21 +2,21 @@
 const { test, expect } = require("@playwright/test");
 
 test("Interceptign the api and updating the response", async ({ page }) => {
-  await page.goto("http://192.168.1.5:8080/");
-  await page.waitForTimeout(3000);
-  await expect(page.locator(".btn")).toBeVisible();
-  await page.locator(".btn").click();
-  await expect(page.locator("h1#name")).toContainText(/Kunal/);
-  await page.waitForTimeout(2000);
   await page.route("http://localhost:3000/api/name", (route) => {
     route.fulfill({
       body: JSON.stringify({ name: "Dinesh" }),
     });
   });
+  await page.goto("http://192.168.1.5:8080/");
+  // await page.waitForTimeout(3000);
+  // await expect(page.locator(".btn")).toBeVisible();
+  // await page.locator(".btn").click();
+  // await expect(page.locator("h1#name")).toContainText(/Kunal/);
+  // await page.waitForTimeout(2000);
   // await page.goto("http://192.168.1.5:8080/");
   // await page.waitForTimeout(5000);
   // await expect(page.locator(".btn")).toBeVisible();
   await page.locator(".btn").click();
   await expect(page.locator("h1#name")).toContainText(/Dinesh/);
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(15000);
 });
